9c895c861a2d5b1303108fb32de8b25284daa7b0
Braian Burini <braianb55@hotmail.com>
2023-09-28 18:40:51 -0300

    creando portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import About from "./components/about/About";
+import Cover from "./components/cover/Cover";
+import Navbar from "./components/navbar/Navbar";
+import { useEffect } from "react";
+import { useState } from "react";
+import Skills from "./components/skills/Skills";
+import Info from "./components/info/Info";
+import Footer from "./components/footer/Footer";
+import Slider from "./components/slider/Slider";
+
 function App() {
-  return <h1>app</h1>;
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollHeight(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, [scrollHeight]);
+
+  return (
+    <div className="App">
+      <Navbar isScrolling={scrollHeight} />
+      <Cover />
+      <section id="about">
+        <About />
+      </section>
+      <section id="proyects">
+        <Slider />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="info">
+        <Info />
+      </section>
+      <section id="contact">
+        <Footer />
+      </section>
+    </div>
+  );
 }
 
 export default App;
