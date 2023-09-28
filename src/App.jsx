d36bdc6a8f7b6fc7c6c1f6778ab990680ef1fe84
Braian Burini <braianb55@hotmail.com>
2023-09-28 19:53:28 -0300

    sadasasdas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import About from "./components/about/About";
+import Cover from "./components/cover/Cover";
+import Navbar from "./components/navbar/Navbar";
 import Skills from "./components/skills/Skills";
+import Info from "./components/info/Info";
 import Footer from "./components/footer/Footer";
 import Slider from "./components/slider/Slider";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <Navbar />
+      <Cover />
       <section id="about">
         <About />
       </section>
@@ -14,6 +19,9 @@ function App() {
       </section>
       <section id="skills">
         <Skills />
+      </section>
+      <section id="info">
+        <Info />
       </section>
       <section id="contact">
         <Footer />
